10454366f01a25427efe552491f7f0edb172a5be
Shrey Kumar <shreyk@chimeratechnologies.com>
2025-06-01 22:28:00 +0530

    ssl false

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,6 +8,7 @@ const connectionPool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: parseInt(process.env.DB_PORT || '5432', 10),
+    ssl: { rejectUnauthorized: false },
 });
 
 connectionPool.on('connect', () => {
